9f1950c42d99076f9a3c3dde0af24a343ed432db
Hanna-Mik <110687941+Hanna-Mik@users.noreply.github.com>
2022-12-07 18:21:29 +0200

    footer component added

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <a href="/">Open-course code,</a> by Hanna Shchepeleva
+    </footer>
+  );
+}
